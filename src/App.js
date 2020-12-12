107b24ab3258159c8f87d3e5ad7111ddb3fb4d2e
Alvaro Sanchez <alvaro.sanchez.galvan@gmail.com>
2020-12-12 13:21:03 +0000

    Google Analytics

--- src/App.js
+++ src/App.js
@@ -22,6 +22,11 @@ import ScrollToTop from "./Components/ScrollToTop";
 import NewClientPage from "./ClientAdmin/NewClientPage";
 import EditClientPage from "./ClientAdmin/EditClientPage";
 import Test from "./ClientAdmin/test";
+//Google Analytics
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-9KWYJNR0M7');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 
 function App() {
   return (
